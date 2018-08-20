e4d6158cd33aeded056002de239a24d26a502b91
Dhanushka <dhanushka@akvasoft.com>
2018-08-20 14:06:41 +0530

    1st commit

---
+++ src/main/java/com/akvasoft/sdms/StudentDatabaseManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.akvasoft.sdms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDatabaseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDatabaseManagementSystemApplication.class, args);
+	}
+}
